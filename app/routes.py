1a037c18091ed3048b79afe6b43a65c911ab2a38
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2020-03-04 11:51:01 +0100

    fixed index redirect

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for(index))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
