85acafda2c37bbfd3b61e81039afbf7177912e20
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2020-03-14 16:12:00 +0100

    Mails are being sent asyncronously

--- app/email.py
+++ app/email.py
@@ -1,13 +1,17 @@
 from flask_mail import Message
 from flask import render_template
 from app import mail, app
+from threading import Thread
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user, email):
