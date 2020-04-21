a0b7db2debccc7def1ded1e7a922dfa6769a80df
SienNuyens <sien.nuyens@student.uantwerpen.be>
2020-04-21 14:12:49 +0200

    sorted routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,11 +2,10 @@ from flask import render_template, redirect
 from flask_login import current_user
 
 from app import db
-from app.models import Statistics, Route, RouteRequest, User
+from app.models import Statistics, Route, RouteRequest, User, RequestStatus
 from app.main import bp
 
 from sqlalchemy import union
-from sqlalchemy import text
 
 from datetime import datetime
 
@@ -32,18 +31,29 @@ def index():
         routes_driver = Route.query.filter_by(driver_id=current_user.id)
         routes_passenger = Route.query.filter(RouteRequest.query.filter_by(user_id=current_user.id, route_id=Route.id).exists())
         routes = routes_driver.union(routes_passenger)
-        future_routes = routes.filter(Route.departure_time >= current_time)
-        #future_routes = future_routes.order_by(text("departure_time"))
+        future_routes_unsort = routes.filter(Route.departure_time >= current_time)
         #order by
+        future_routes = []
+        future_routes.append(future_routes_unsort.first())
+        for unsort_route in future_routes_unsort:
+            temp_routes = []
+            for route in future_routes:
+                if route.departure_time > unsort_route.departure_time:
+                    temp_routes.append(unsort_route)
+
+                temp_routes.append(route)
+            future_routes = temp_routes
 
         passengerIds = []
-        if future_routes.first():
-            passengerIds = future_routes.first().passengers()
+        if future_routes[0]:
+            passengerIds = future_routes[0].passengers()
         passengers = []
         for passengerId in passengerIds:
+            # get the users of the passenger id
             user = User.query.filter_by(id=passengerId).first_or_404()
             passengers.append(user)
-        return render_template('main/main_logged_in.html', title='Dashboard', future_routes=future_routes, passengers= passengers)
+
+        return render_template('main/main_logged_in.html', title='Dashboard', future_routes=future_routes, passengers=passengers)
     return render_template('main/home.html', title='Welcome')
 
 
